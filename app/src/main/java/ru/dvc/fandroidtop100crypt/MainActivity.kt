10058e82d25ae7aab0470b46681a4c96fe70c569
Archi39 <ey.samarin@gmail.com>
2019-03-31 17:53:59 +0300

    #FTR add about activity format

--- app/src/main/java/ru/dvc/fandroidtop100crypt/MainActivity.kt
+++ app/src/main/java/ru/dvc/fandroidtop100crypt/MainActivity.kt
@@ -1,12 +1,34 @@
 package ru.dvc.fandroidtop100crypt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import ru.dvc.fandroidtop100crypt.activities.AboutActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item!!.itemId) {
+            R.id.action_about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/ru/dvc/fandroidtop100crypt/activities/AboutActivity.kt
@@ -0,0 +1,22 @@
+package ru.dvc.fandroidtop100crypt.activities
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_about.*
+import ru.dvc.fandroidtop100crypt.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        buttonRateApp.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${applicationContext.packageName}")))
+        }
+    }
+}
